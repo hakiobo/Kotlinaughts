f586578ce462cb9268ef8d1c34bf1451c57b9fc1
Jacob Huebner <hakiobo7@gmail.com>
2022-02-04 00:18:23 -0600

    Added Disjoint Spare Table

---
+++ DisjointMinTable.kt
@@ -0,0 +1,33 @@
+private class DisjointMinTable(input: IntArray) {
+    val n = Integer.highestOneBit((input.size shl 1) - 1)
+    val levels = Integer.numberOfTrailingZeros(n)
+    val nums = IntArray(n) { idx -> if (idx < input.size) input[idx] else 0 }
+    val table = Array(levels) { IntArray(n) }
+
+    init {
+        for (level in 0 until levels) {
+            for (block in 0 until (1 shl level)) {
+                val start = block shl (levels - level)
+                val end = (block + 1) shl (levels - level)
+                val mid = (start + end) shr 1
+
+                table[level][mid] = nums[mid]
+                for (x in mid + 1 until end) {
+                    table[level][x] = min(table[level][x - 1], nums[x])
+                }
+
+                table[level][mid - 1] = nums[mid - 1]
+                for (x in mid - 2 downTo start) {
+                    table[level][x] = min(table[level][x + 1], nums[x])
+                }
+            }
+        }
+    }
+
+    fun getMinRange(start: Int, end: Int): Int {
+        if (end < start) return getMinRange(start, end)
+        if (start == end) return nums[start]
+        val level = levels - 1 - Integer.numberOfTrailingZeros(Integer.highestOneBit(start xor end))
+        return min(table[level][start], table[level][end])
+    }
+}
