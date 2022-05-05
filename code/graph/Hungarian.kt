808c567a1a27ba32baa9a9ee795c6ee23cc91615
Jacob Huebner <hakiobo7@gmail.com>
2022-05-04 19:43:30 -0500

    Update Hungarian.kt

--- code/graph/Hungarian.kt
+++ code/graph/Hungarian.kt
@@ -4,9 +4,8 @@
 private class Hungarian(val costs: Array<IntArray>) { // assuming a square matrix of costs
     val n = costs.size
     var adjustment = 0
-    val labelByRow = IntArray(n)
-    val labelByCol = IntArray(n)
-    var nextRowToMatch = -1
+    val adjustmentByRow = IntArray(n)
+    val adjustmentByCol = IntArray(n)
 
     val minSlackRowByCol = IntArray(n)
     val minSlackValueByCol = IntArray(n)
@@ -16,30 +15,54 @@ private class Hungarian(val costs: Array<IntArray>) { // assuming a square matri
     val matchColByRow = IntArray(n) { -1 }
     val matchRowByCol = IntArray(n) { -1 }
 
-    fun execute(): Int { // returns the final cost, can get the arrangements from matchRowByCol an matchColByRow
-        reduce()
-        greedyMatch()
+    fun findBestMatching(): Int { // returns the final cost, can get the arrangements from matchRowByCol an matchColByRow
 
-        var row = fetchUnmatchedRow()
-        while (row != n) {
-            initializePhase(row)
-            executePhase()
-            row = fetchUnmatchedRow()
+        // get some values down to 0 to start
+        for (row in 0 until n) {
+            var low = costs[row][0]
+            for (col in 1 until n) {
+                low = min(adjustmentByRow[row], costs[row][col])
+            }
+            adjustment += low
+            adjustmentByRow[row] = low
+        }
+        adjustmentByCol.fill(Int.MAX_VALUE)
+        for (row in 0 until n) {
+            for (col in 0 until n) {
+                adjustmentByCol[col] = min(costs[row][col] - adjustmentByRow[row], adjustmentByCol[col])
+            }
+        }
+        adjustment += adjustmentByCol.sum()
+
+        // greedily set up some matches
+        for (row in 0 until n) {
+            for (col in 0 until n) {
+                if (matchColByRow[row] == -1 && matchRowByCol[col] == -1 && costs[row][col] - adjustmentByRow[row] - adjustmentByCol[col] == 0) {
+                    matchRowByCol[col] = row
+                    matchColByRow[row] = col
+                }
+            }
+        }
+
+        for (row in 0 until n) {
+            if (matchColByRow[row] == -1) {
+                // set some stuff up
+                parentRowByCommittedCol.fill(-1)
+                committedRows[0] = row
+                numCommittedRows = 1
+                minSlackRowByCol.fill(row)
+                for (col in 0 until n) {
+                    minSlackValueByCol[col] = costs[row][col] - adjustmentByRow[row] - adjustmentByCol[col]
+                }
+
+                executePhase()
+            }
         }
         return adjustment
     }
 
-    inline fun initializePhase(row: Int) {
-        numCommittedRows = 0
-        parentRowByCommittedCol.fill(-1)
-        committedRows[numCommittedRows++] = row
-        for (col in 0 until n) {
-            minSlackValueByCol[col] = costs[row][col] - labelByRow[row] - labelByCol[col]
-            minSlackRowByCol[col] = row
-        }
-    }
 
-    inline fun executePhase() {
+    fun executePhase() {
         while (true) {
             var minSlackRow = -1
             var minSlackCol = -1
@@ -52,14 +75,28 @@ private class Hungarian(val costs: Array<IntArray>) { // assuming a square matri
                 }
             }
             if (minSlackValue > 0) {
-                updateLabeling(minSlackValue)
+                adjustment += minSlackValue
+                for (i in 0 until numCommittedRows) {
+                    adjustmentByRow[committedRows[i]] += minSlackValue
+                }
+                for (col in 0 until n) {
+                    if (parentRowByCommittedCol[col] == -1) {
+                        minSlackValueByCol[col] -= minSlackValue
+                    } else {
+                        adjustmentByCol[col] -= minSlackValue
+                    }
+                }
             }
+
             parentRowByCommittedCol[minSlackCol] = minSlackRow
             if (matchRowByCol[minSlackCol] == -1) {
                 var committedCol = minSlackCol
                 while (committedCol != -1) {
-                    val temp = matchColByRow[parentRowByCommittedCol[committedCol]]
-                    match(parentRowByCommittedCol[committedCol], committedCol)
+                    val row = parentRowByCommittedCol[committedCol]
+
+                    val temp = matchColByRow[row]
+                    matchRowByCol[committedCol] = row
+                    matchColByRow[row] = committedCol
                     committedCol = temp
                 }
                 return
@@ -68,68 +105,13 @@ private class Hungarian(val costs: Array<IntArray>) { // assuming a square matri
                 committedRows[numCommittedRows++] = row
                 for (col in 0 until n) {
                     if (parentRowByCommittedCol[col] == -1) {
-                        val slack = costs[row][col] - labelByRow[row] - labelByCol[col]
+                        val slack = costs[row][col] - adjustmentByRow[row] - adjustmentByCol[col]
                         if (minSlackValueByCol[col] > slack) {
                             minSlackValueByCol[col] = slack
                             minSlackRowByCol[col] = row
                         }
                     }
                 }
-            }
-        }
-    }
-
-    fun reduce() {
-        for (row in 0 until n) {
-            var low = costs[row][0]
-            for (col in 1 until n) {
-                low = min(labelByRow[row], costs[row][col])
-            }
-            adjustment += low
-            labelByRow[row] = low
-        }
-
-        labelByCol.fill(Int.MAX_VALUE)
-        for (row in 0 until n) {
-            for (col in 0 until n) {
-                labelByCol[col] = min(costs[row][col] - labelByRow[row], labelByCol[col])
-            }
-        }
-        adjustment += labelByCol.sum()
-    }
-
-    inline fun greedyMatch() {
-        for (row in 0 until n) {
-            for (col in 0 until n) {
-                if (matchColByRow[row] == -1 && matchRowByCol[col] == -1 && costs[row][col] - labelByRow[row] - labelByCol[col] == 0) {
-                    match(row, col)
-                }
-            }
-        }
-    }
-
-    inline fun fetchUnmatchedRow(): Int {
-        while (++nextRowToMatch != n && matchColByRow[nextRowToMatch] != -1) continue
-        return nextRowToMatch
-    }
-
-
-    inline fun match(row: Int, col: Int) {
-        matchRowByCol[col] = row
-        matchColByRow[row] = col
-    }
-
-    inline fun updateLabeling(slack: Int) {
-        for(i in 0 until numCommittedRows) {
-            adjustment += slack
-            labelByRow[committedRows[i]] += slack
-        }
-        for (col in 0 until n) {
-            if (parentRowByCommittedCol[col] == -1) {
-                minSlackValueByCol[col] -= slack
-            } else {
-                adjustment -= slack
-                labelByCol[col] -= slack
             }
         }
     }
