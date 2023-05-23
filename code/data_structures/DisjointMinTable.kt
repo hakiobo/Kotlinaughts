f47057359505895a46d68000a3eb2d502dd8e862
Jacob Huebner <hakiobo7@gmail.com>
2023-05-23 17:08:23 -0500

    Update DisjointMinTable.kt
    kotlin 1.4 update

--- code/data_structures/DisjointMinTable.kt
+++ code/data_structures/DisjointMinTable.kt
@@ -1,6 +1,6 @@
 private class DisjointMinTable(input: IntArray) {
-    val n = Integer.highestOneBit((input.size shl 1) - 1)
-    val levels = Integer.numberOfTrailingZeros(n)
+    val n = ((input.size shl 1) - 1).takeHighestOneBit()
+    val levels = n.countTrailingZeroBits()
     val nums = IntArray(n) { idx -> if (idx < input.size) input[idx] else 0 }
     val table = Array(levels) { IntArray(n) }
 
@@ -27,7 +27,7 @@ private class DisjointMinTable(input: IntArray) {
     fun getMinRange(start: Int, end: Int): Int {
         if (end < start) return getMinRange(end, start)
         if (start == end) return nums[start]
-        val level = levels - 1 - Integer.numberOfTrailingZeros(Integer.highestOneBit(start xor end))
+        val level = levels - 1 - (start xor end).takeHighestOneBit().countTrailingZeroBits()
         return min(table[level][start], table[level][end])
     }
 }
