f862cc5e7fb6a81715d6ea8ccf7fb08833a58173
Jacob Huebner <hakiobo7@gmail.com>
2023-06-06 04:21:47 -0500

    Update SparseAndTable.kt

--- code/data_structures/SparseAndTable.kt
+++ code/data_structures/SparseAndTable.kt
@@ -3,15 +3,10 @@ private class SparseAndTable(nums: LongArray) {
     val table = Array(n.takeHighestOneBit().countTrailingZeroBits() + 1) { LongArray(n) }
 
     init {
-        for (x in 0 until n) {
-            table[0][x] = nums[x]
-        }
+        for (x in 0 until n) table[0][x] = nums[x]
         for (bit in 1 until table.size) {
-            val pow = 1 shl bit
-            var x = 0
-            while (x + pow <= n) {
-                table[bit][x] = table[bit - 1][x] and table[bit - 1][x + (pow shr 1)]
-                x++
+            for (x in 0..n - (1 shl bit)) {
+                table[bit][x] = table[bit - 1][x] and table[bit - 1][x + (1 shl (bit - 1))]
             }
         }
     }
@@ -22,8 +17,7 @@ private class SparseAndTable(nums: LongArray) {
             val high = dif.takeHighestOneBit()
             val bit = high.countTrailingZeroBits()
             table[bit][start] and table[bit][end - high + 1]
-        } else {
-            getANDRange(start, n - 1) and getANDRange(0, end)
-        }
+        } else 0
+
     }
 }
